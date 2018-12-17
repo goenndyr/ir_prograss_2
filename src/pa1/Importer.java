270c2caaf15996bd6af484254f0aa32056652327
Thomas Spanier <thomas.spanier24@googlemail.com>
2018-12-17 23:22:27 +0100

    added 2.3 and 2.4

--- src/pa1/Importer.java
+++ src/pa1/Importer.java
@@ -8,16 +8,24 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Imports files and directories into our index.
  *
  * @author Klaus Berberich (klaus.berberich@htwsaar.de)
+ * @author Benjamin Lautenberg
+ * @author Phillip Persch
+ * @author Thomas Spanier
  */
 public class Importer {
 
 	static final String dbPath = "nyt.sqlite";
 	
+	/**
+	 * imports all files in directory
+	 * @param root
+	 */
 	private void importDirectory(File root) {
 		LinkedList<File> files = new LinkedList<File>();
 		File db = new File(dbPath);
@@ -25,11 +33,10 @@ public class Importer {
 		Parser parser = new Parser();
 		PreparedStatement insertIntoDocs = null;
 		PreparedStatement insertIntoTfs = null;
-		int batchCount = 0;
+		int i = 0;
 		
 		traverse(root, files);
 		try {
-            //Class.forName("org.sqlite.JDBC");
             conn = DriverManager.getConnection("jdbc:sqlite:" + db);
             conn.setAutoCommit(false);
             
@@ -37,8 +44,7 @@ public class Importer {
             insertIntoTfs = conn.prepareStatement("INSERT INTO tfs (did, term, tf) VALUES (?, ?, ?)");
             
             for(File file: files) {
-            	batchCount = 0;
-    			Document doc = parser.parse(file);
+            	Document doc = parser.parse(file);
     			System.out.println(doc.getId());
     			
     			//Insert into docs
@@ -46,6 +52,7 @@ public class Importer {
     			insertIntoDocs.setString(2, doc.getTitle());
     			insertIntoDocs.setString(3, doc.getURL());
     			insertIntoDocs.addBatch();
+    			
     			
     			//insert into tfs-HashMap
     			HashMap<String, Integer> tfs = new HashMap<String, Integer>();
@@ -65,18 +72,30 @@ public class Importer {
                     insertIntoTfs.addBatch();
                 }
     			
-    			if(++batchCount % 10 == 0) {
+    			if(++i % 10 == 0) {
+    				System.out.println("i: " + i);
     				insertIntoDocs.executeBatch();
     				insertIntoTfs.executeBatch();
     			}
     		}
-            /*
-            if(batchCount % 10 != 0) {
+            //Execute rest
+            if(i % 10 != 0) {
             	insertIntoDocs.executeBatch();
     			insertIntoTfs.executeBatch();
-            }*/
+            }
             
-    		
+            String dls = "CREATE TABLE dls AS SELECT did, SUM(tf) AS len FROM tfs GROUP BY did;";
+            PreparedStatement createDls = conn.prepareStatement(dls);
+            createDls.execute();
+            
+            String dfs = "CREATE TABLE dfs AS SELECT term, COUNT(did) AS df FROM tfs GROUP BY term;"; 
+            PreparedStatement createDfs = conn.prepareStatement(dfs);
+            createDfs.execute();
+            
+            String d = "CREATE TABLE d AS SELECT COUNT(*) as size FROM docs;";
+            PreparedStatement createD = conn.prepareStatement(d);
+            createD.execute();
+
             conn.commit();
             conn.close();
 		} catch (Exception e) {
@@ -87,24 +106,33 @@ public class Importer {
 		
 	}
 	
-	private void deleteAll() {
-		
+	/**
+	 * clears database
+	 * clears all entries in docs and tfs
+	 * drops databases dls, dfs and d
+	 */
+	private void clearAll() {
 		File db = new File(dbPath);
 		Connection conn;
 		try {
 			conn = DriverManager.getConnection("jdbc:sqlite:" + db);
 			PreparedStatement deleteFromDocs = conn.prepareStatement("DELETE FROM docs");
 			PreparedStatement deleteFromTfs = conn.prepareStatement("DELETE FROM tfs");
+			PreparedStatement deleteFromDls = conn.prepareStatement("DROP TABLE dls");
+			PreparedStatement deleteFromDfs = conn.prepareStatement("DROP TABLE dfs");
+			PreparedStatement deleteFromD = conn.prepareStatement("DROP TABLE d");
 	        conn.setAutoCommit(false);
 	        
 	        deleteFromDocs.execute();
 	        deleteFromTfs.execute();
+	        deleteFromDls.execute();
+	        deleteFromDfs.execute();
+	        deleteFromD.execute();
 	        conn.commit();
 	        conn.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		} 
 		
         
 	}
@@ -140,20 +168,42 @@ public class Importer {
         }
     }
 
-    public static void main(String[] args) {
-    	
+    /**
+     * Starts the program
+     * Input c to clear database, i for insert values into database or exit to exit program
+     */
+    private void start() {
+    	final String MSG_USAGE = "c for clear, i for insert, exit to exit";
+    	final String MSG_START = "Input: ";
     	String nytPath = "nyt/data";
-    	
-    	/*File tmp = new File(dbPath);
-    	System.out.println(tmp.getAbsolutePath());
-    	*/
-        Importer importer = new Importer();
-        
-        if(args.length > 0 && args[0].equals("-c")) {
-        	importer.deleteAll();
-    	} else {
-    		importer.importDirectory(new File(nytPath));
-    	}
+    	 
+        Scanner in = new Scanner(System.in);
+		while (true) {
+			System.out.print(MSG_START);
+			String tmp = in.nextLine();
+			
+			switch(tmp) {
+				case "c":
+					clearAll();
+		        	System.out.println("cleared db");
+					break;
+				case "i":
+					importDirectory(new File(nytPath));
+					break;
+				case "exit":
+					in.close();
+					System.exit(0);
+					break;
+				default:
+					System.out.println(MSG_USAGE);
+			}
+	        
+		}
+    }
+    
+    
+    public static void main(String[] args) {
+    	new Importer().start();
     }
 
 }
